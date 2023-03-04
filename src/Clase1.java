b6a16d800d85acf340475a8a6eff78adba9e0de1
AbrahamHub <apple_abraham@hotmail.com>
2023-03-04 16:44:40 -0600

    #3

---
+++ src/Clase1.java
@@ -0,0 +1,10 @@
+package src;
+
+import javax.swing.JOptionPane;
+
+public class Clase1 {
+    public static void main(String[] args) {
+        System.out.println(" Hola Mundo!");
+        JOptionPane.showMessageDialog(null,"Hola");
+    }
+}
